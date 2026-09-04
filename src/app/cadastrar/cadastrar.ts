a4d8f9bef153de64d422e4c1e0cd145da7de8178
Vinícius Zanela <vinicius.r.zanela@gmail.com>
2026-09-04 16:10:54 -0300

    Tela de cadastro pronta

--- src/app/cadastrar/cadastrar.ts
+++ src/app/cadastrar/cadastrar.ts
@@ -12,18 +12,34 @@ import { FormsModule } from '@angular/forms';
 
 export class Cadastrar 
 {
+  nome = '';
+  nomeMeio ='';
+  sobrenome = '';
+  cpf = '';
+  email = '';
   mostrar = false;
   esconder = false;
-  cpf = '';
   senha = '';
   confirmar = '';
-  Validar(cpf: string): boolean
+  tentativa = false;
+
+  vNome(valor: string): boolean
   {
-    const numeros = cpf.replace(/\D/g, '');
-    if (numeros.length !== 11)
+    return /^[\p{L}\s]+$/u.test(valor.trim());
+  }
+  vEmail(valor: string): boolean
+  {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(valor.trim());
+  }
+  Validar(cpf: string): boolean
+{
+    cpf = cpf.trim();
+    const formato = /^\d{11}$|^\d{3}\.\d{3}\.\d{3}-\d{2}$/;
+    if (!formato.test(cpf))
     {
       return false;
     }
+    const numeros = cpf.replace(/\D/g, '');
     if (/^(\d)\1{10}$/.test(numeros))
     {
       return false;
@@ -38,11 +54,34 @@ export class Cadastrar
       const resto = (soma * 10) % 11;
       return resto === 10 ? 0 : resto;
     };
-    return (calcular(9) === Number(numeros[9]) && calcular(10) === Number(numeros[10]));
+    return (
+    calcular(9) === Number(numeros[9]) &&
+    calcular(10) === Number(numeros[10])
+    );
+  }
+  get nomeV()
+  {
+    return this.vNome(this.nome);
+  }
+  get nomeMeioV()
+  {
+    return !this.nomeMeio.trim() || this.vNome(this.nomeMeio)
+  }
+  get sobrenomeV()
+  {
+    return this.vNome(this.sobrenome);
   }
   get valido()
   {
     return this.Validar(this.cpf);
+  }
+  get emailV()
+  {
+    return this.vEmail(this.email);
+  }
+  get senhaV()
+  {
+    return this.senha.trim().length >= 6;
   }
   get iguais()
   {
