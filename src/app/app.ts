db091222da809adc5ac160c3280a8e84d9929acb
Vinícius Zanela <vinicius.r.zanela@gmail.com>
2026-09-06 23:12:21 -0300

    atualiza o player de musica

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
-@Component
-({
+@Component({
   selector: 'app-root',
   imports: [RouterLink, RouterOutlet],
   templateUrl: './app.html',
@@ -14,87 +13,32 @@ export class App
   protected readonly title = signal('eletronicos');
   constructor(public router: Router){}
   audio?: HTMLAudioElement;
-  musicaTocando = false;
-  tempoAtual = 0;
-  duracaoMusica = 0;
-  musicaAtual = 0;
-  musicas = ['musica1.mp3', 'musica2.mp3', 'musica3.mp3', 'musica4.mp3', 'musica5.mp3'];
-  toggleMusica()
-  {
-    if (!this.audio)
-    {
-        this.tocarMusica();
-        return;
-    }
-    if (this.audio.paused)
-    {
-        this.audio.play();
-        this.musicaTocando = true;
-    }
-    else
-    {
-        this.audio.pause();
-        this.musicaTocando = false;
-    }
-  }
-  voltarMusica()
-  {
-    if (!this.audio)
-    {
-        return;
-    }
-    if (this.audio.currentTime > 3)
-    {
-        this.audio.currentTime = 0;
-        this.tempoAtual = 0;
-    }
-    else
-    {
-    this.musicaAtual--;
-    if (this.musicaAtual < 0)
-    {
-        this.musicaAtual = this.musicas.length - 1;
-    }
-    this.tocarMusica();
-    }
-  }
-  proximaMusica()
-  {
-    this.musicaAtual++;
-    if (this.musicaAtual >= this.musicas.length)
-    {
-        this.musicaAtual = 0;
-    }
-    this.tocarMusica();
-  }
-
-  alterarTempo(event: Event)
-  {
-    const valor = Number((event.target as HTMLInputElement).value);
-    this.tempoAtual = valor;
-    if (this.audio)
-    {
-        this.audio.currentTime = valor;
-    }
-  }
+  musica = 'musica1.mp3';
+  mutado = false;
+  saudacoes = true;
   tocarMusica()
   {
     this.audio?.pause();
-    this.audio = new Audio(this.musicas[this.musicaAtual]);
-    this.audio.addEventListener('loadedmetadata', () =>
+    this.audio = new Audio(this.musica);
+    this.audio.loop = true;
+    this.audio.muted = this.mutado;
+    this.audio.play()
+    .catch(() =>
     {
-        this.duracaoMusica = this.audio!.duration;
+      console.log('Não foi possível iniciar a música.');
     });
-    this.audio.addEventListener('timeupdate', () =>
+  }
+  toggleMudo()
+  {
+    this.mutado = !this.mutado;
+    if (this.audio)
     {
-        this.tempoAtual = this.audio!.currentTime;
-    });
-    this.audio.addEventListener('ended', () =>
-    {
-        this.proximaMusica();
-    });
-    this.audio.play();
-    this.musicaTocando = true;
-    this.tempoAtual = 0;
+      this.audio.muted = this.mutado;
+    }
+  }
+  fSaudacoes()
+  {
+    this.saudacoes = false;
+    this.tocarMusica();
   }
 }
