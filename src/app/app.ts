125c62f66da6f5332801e5633f5d6bd398b38d6a
Vinícius Zanela <vinicius.r.zanela@gmail.com>
2026-09-07 22:25:03 -0300

    muda alguns textos da página do carrinho e adiciona um gif de fundo em todas as telas

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,35 +10,35 @@ styleUrl: './app.css'
 
 export class App
 {
-protected readonly title = signal('eletronicos');
-constructor(public router: Router){}
-audio?: HTMLAudioElement;
-musica = 'musica1.mp3';
-mutado = false;
-saudacoes = true;
-tocarMusica()
-{
-this.audio?.pause();
-this.audio = new Audio(this.musica);
-this.audio.loop = true;
-this.audio.muted = this.mutado;
-this.audio.play()
-.catch(() =>
-{
-console.log('Não foi possível iniciar a música.');
-});
-}
-toggleMudo()
-{
-this.mutado = !this.mutado;
-if (this.audio)
-{
-this.audio.muted = this.mutado;
-}
-}
-fSaudacoes()
-{
-this.saudacoes = false;
-this.tocarMusica();
-}
+    protected readonly title = signal('eletronicos');
+    constructor(public router: Router){}
+    audio?: HTMLAudioElement;
+    musica = 'musica1.mp3';
+    mutado = false;
+    saudacoes = true;
+    tocarMusica()
+    {
+        this.audio?.pause();
+        this.audio = new Audio(this.musica);
+        this.audio.loop = true;
+        this.audio.muted = this.mutado;
+        this.audio.play()
+        .catch(() =>
+        {
+            console.log('Não foi possível iniciar a música.');
+        });
+    }
+    toggleMudo()
+    {
+        this.mutado = !this.mutado;
+        if (this.audio)
+        {
+            this.audio.muted = this.mutado;
+        }
+    }
+    fSaudacoes()
+    {
+        this.saudacoes = false;
+        this.tocarMusica();
+    }
 }
